94812abaa6bf147f9a92865ca9a61ca777b4b1d5
Mark B DSouza <mark.benjamin.dsouza@gmail.com>
2020-07-18 21:42:01 +0530

    sort logic

--- scripts/index.js
+++ scripts/index.js
@@ -12,6 +12,7 @@ const methodEl = document.getElementById('method');
 const hostNameEl = document.getElementById('hostName');
 const facilityEl = document.getElementById('facility');
 const errorMsgEl = document.getElementById('errorMsg');
+const sortEl = document.querySelectorAll('.arrow');
 const DEFAULT_OPTION_VALUE = 'EMPTY';
 
 // https://github.com/florinpop17/app-ideas/blob/master/Projects/3-Advanced/NASA-Exoplanet-Query.md
@@ -20,10 +21,11 @@ const DEFAULT_OPTION_VALUE = 'EMPTY';
 // <br>
 
 // on load
+let arrayData;
 async function init() {
-  let data = await initiateSearch({ isDefaultSearch: true });
+  arrayData = await initiateSearch({ isDefaultSearch: true });
   displayElementsAfterLoad();
-  initLoad(data);
+  initLoad(arrayData);
 }
 
 function displayElementsAfterLoad() {
@@ -41,12 +43,15 @@ function initLoad(data) {
   let innerHtml = '';
   data.forEach((row, index) => {
     if (index > 0) {
-      innerHtml += `<tr>
-      
+      innerHtml += `<tr>      
       <td class="data__col">${row[1]}</td>
       <td class="data__col">${row[2]}</td>
       <td class="data__col">${row[3]}</td>
-      <td class="data__col">${row[0]}</td>
+      <td class="data__col"><a href="https://exoplanetarchive.ipac.caltech.edu/cgi-bin/DisplayOverview/nph-DisplayOverview?objname=${row[4]
+        .replace('+', '%2B')
+        .replace(' ', '+')}" target="_blank">
+      ${row[0]}
+      </a></td>
     </tr>
       `;
       years.add(row[1]);
@@ -76,7 +81,7 @@ function addOptionToElement(element, optionArray) {
 function createURL() {
   let isClauseAdded = false;
   let URL =
-    'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_hostname,pl_disc,pl_discmethod,pl_facility&order=pl_disc';
+    'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_hostname,pl_disc,pl_discmethod,pl_facility,pl_name&order=pl_disc';
   if (
     yearEl.value !== DEFAULT_OPTION_VALUE ||
     methodEl !== DEFAULT_OPTION_VALUE ||
@@ -111,7 +116,7 @@ async function initiateSearch({ isDefaultSearch }) {
   if (isDefaultSearch) {
     //
     URL =
-      'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_hostname,pl_disc,pl_discmethod,pl_facility&order=pl_disc';
+      'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_hostname,pl_disc,pl_discmethod,pl_facility,pl_name&order=pl_disc';
   } else {
     URL = createURL();
   }
@@ -130,14 +135,18 @@ function validate() {
 
 function updateTable(data) {
   let innerHtml = '';
+  arrayData = data;
   data.forEach((row, index) => {
     if (index > 0) {
-      innerHtml += `<tr>
-      
+      innerHtml += `<tr>      
       <td class="data__col">${row[1]}</td>
       <td class="data__col">${row[2]}</td>
       <td class="data__col">${row[3]}</td>
-      <td class="data__col">${row[0]}</td>
+      <td class="data__col"><a href="https://exoplanetarchive.ipac.caltech.edu/cgi-bin/DisplayOverview/nph-DisplayOverview?objname=${row[4]
+        .replace('+', '%2B')
+        .replace(' ', '+')}" target="_blank">
+      ${row[0]}
+      </a></td>       
     </tr>
       `;
     }
@@ -147,6 +156,7 @@ function updateTable(data) {
 }
 
 async function validateAndSearch() {
+  sortEl.forEach((el) => el.classList.remove('large'));
   if (validate()) {
     errorMsgEl.classList.add('hidden');
     let data = await initiateSearch({ isDefaultSearch: false });
@@ -157,11 +167,42 @@ async function validateAndSearch() {
   }
 }
 
+function sortData(e) {
+  sortEl.forEach((el) => el.classList.remove('large'));
+  e.currentTarget.classList.add('large');
+
+  let sortIndex;
+  let text = e.currentTarget.closest('.data__col').innerHTML;
+  sortIndex = text.includes('Year')
+    ? 1
+    : text.includes('Method')
+    ? 2
+    : text.includes('Facility')
+    ? 3
+    : 0;
+  let sortOrder = e.currentTarget.classList.contains('up');
+  arrayData.sort((a, b) => {
+    return sortOrder
+      ? a[sortIndex] > b[sortIndex]
+        ? 1
+        : -1
+      : a[sortIndex] > b[sortIndex]
+      ? -1
+      : 1;
+  });
+  updateTable(arrayData);
+}
 //event listeners
 
 clearBtn.addEventListener('click', () => {
   inputEls.forEach((el) => (el.value = ''));
 });
+
+sortEl.forEach((el) => el.addEventListener('click', sortData));
 searchBtn.addEventListener('click', validateAndSearch);
 initiateSearch();
 init();
+
+// https://exoplanetarchive.ipac.caltech.edu/cgi-bin/DisplayOverview/nph-DisplayOverview?objname=PSR%20B1257+12%20c
+// https://exoplanetarchive.ipac.caltech.edu/cgi-bin/DisplayOverview/nph-DisplayOverview?objname=PSR+B1257%2B12+b
+// https://exoplanetarchive.ipac.caltech.edu/cgi-bin/DisplayOverview/nph-DisplayOverview?objname=PSR+B1257+12%20b
